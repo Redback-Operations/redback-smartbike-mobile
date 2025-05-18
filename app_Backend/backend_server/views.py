7b62b17d191d61d001bf6cf0b6dc90cdfaee536d
TharukaN99 <163091374+TharukaN99@users.noreply.github.com>
2025-05-18 22:55:24 +1000

    Update views.py reslove conflict

--- app_Backend/backend_server/views.py
+++ app_Backend/backend_server/views.py
@@ -547,7 +547,6 @@ def password_reset_new_password(request):
 def getDebugMode():
     return os.getenv('DEBUG','').strip().upper() == 'TRUE'
 
-
 # --- Schedule Views ---
 @api_view(['POST'])
 def create_schedule(request):
@@ -556,7 +555,6 @@ def create_schedule(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 @api_view(['GET'])
 def get_schedules(request, email):
     try:
